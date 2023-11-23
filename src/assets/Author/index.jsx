94f5affd4ab8116b4e9fdda64dbb8143d75c505b
jshtengker <leonardotengker@gmail.com>
2023-11-23 23:51:19 +0800

    commiting project

--- src/assets/Author/index.jsx
+++ src/assets/Author/index.jsx
@@ -1,9 +1,21 @@
-
+import { getDatabase, ref, onValue, child, get } from "firebase/database";
+import { useEffect, useState } from "react";
 
 const Author = () => {
+  const [image3, setImage3] = useState("");
+
+useEffect(() => {  
+    const db = getDatabase();
+    const aboutRef = ref(db, "about/");
+    onValue(aboutRef, (snapshot) => {
+      const data = snapshot.val();
+      setImage3(data.image3);
+    });
+  }, []);
+
   return (
     <div className="image">
-    <a href="#"><img src="src/assets/images/AuthorProfile.jpg" alt /></a>
+    <a href="#"><img src={image3} alt /></a>
     <div className="author-content">
           <h4>Joshua Tengker</h4>
           <span>Student</span>
--- src/assets/MyWork/index.jsx
+++ src/assets/MyWork/index.jsx
@@ -1,5 +1,20 @@
+import { getDatabase, ref, onValue, child, get } from "firebase/database";
+import { useEffect, useState } from "react";
 
 const MyWork = () => {
+  const [image4, setImage4] = useState("");
+  const [image5, setImage5] = useState("");
+
+  useEffect(() => {
+    const db = getDatabase();
+    const aboutRef = ref(db, "about/");
+    onValue(aboutRef, (snapshot) => {
+      const data = snapshot.val();
+      setImage4(data.image4);
+      setImage5(data.image5);
+    });
+  }, []);
+
   return (
     <section className="section my-work" id="section3">
     <div className="container">
@@ -23,9 +38,9 @@ const MyWork = () => {
           <div className="isotope-box">
             <div className="isotope-item" data-type="nature">
               <figure className="snip1321">
-                <img src="src/assets/images/HackerRankJSBasic.png" alt="sq-sample26" />
+                <img src={image4} alt="sq-sample26" />
                 <figcaption>
-                  <a href="src/asssets/images/portfolio-01.jpg" data-lightbox="image-1" data-title="Caption"><i className="fa fa-search" /></a>
+                  <a href={image4} data-lightbox="image-1" data-title="Caption"><i className="fa fa-search" /></a>
                   <h4>HackerRank Basic Javascript Certificate</h4>
                   <span></span>
                 </figcaption>
@@ -33,9 +48,9 @@ const MyWork = () => {
             </div>
             <div className="isotope-item" data-type="people">
               <figure className="snip1321">
-                <img src="src/assets/images/lambo.jpeg" alt="sq-sample26" />
+                <img src={image5} alt="sq-sample26" />
                 <figcaption>
-                  <a href="src/assets/images/portfolio-02.jpg" data-lightbox="image-1" data-title="Caption"><i className="fa fa-search" /></a>
+                  <a href={image5} data-lightbox="image-1" data-title="Caption"><i className="fa fa-search" /></a>
                   <h4>Lamborghini Revuelto</h4>
                   <span></span>
                 </figcaption>
